4630713694f33cc4fee3a5236d762f50f4ea28cf
ktorres97 <31709555+ktorres97@users.noreply.github.com>
2019-10-15 12:22:39 -0700

    Update ai.py

--- MineSweeper/ai.py
+++ MineSweeper/ai.py
@@ -18,3 +18,63 @@ If there are no obvious choices, either move to an adjacent unsolved cell and/or
 begin to assign probabilities of the adjacent cells being mines
 
 '''
+class AI():
+    def __init__():
+        self.pos = None
+        self.target_x = None
+        self.target_y = None
+        self.targets = None
+        self.flag_target = False
+        self.total_arrangements = None
+
+    def run_ai(self):
+        self.mine_logic()
+
+    def pick_next_target(self):
+        pass
+
+    def check_linked(self):
+        pass
+
+    def pick_random_target(self):
+        pass
+
+    def move(self):
+        pass
+
+    def add_hidden_near(self):
+        pass
+
+    def pick_target(self):
+        pass
+
+    def assign_probabilities(self):
+        pass
+
+    def combination_amount(self):
+        pass
+
+    def factorial(self):
+        pass
+
+    def determine_solutions(self):
+        pass
+
+    def validate_solution(self):
+        pass
+
+    def violated_a(self):
+        pass
+
+    def violated_b(self):
+        #there's two for some reason?
+        pass
+
+    def add_connected(self):
+        pass
+
+    def in_section(self):
+        pass
+
+    def mine_logic(self):
+        pass
